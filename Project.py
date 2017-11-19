6f026b20cd2bfab707c30e17b072678c7797a35f
mostafa mohamed <mostafamousa1080p@gmail.com>
2017-11-19 18:33:44 +0200

    Working On GUI and File

--- Project.py
+++ Project.py
@@ -1,44 +1,73 @@
 import tkinter as tk
 from functools import partial
+#####Working With File#######
+# Searching in File
+Check = False
+def FileSearch(Fristequ, SecondEqu, Operation, Ruselt):
+    global Check
+    file = open("Math.txt", "r")
+    Holder = str(Fristequ) + str(Operation) + str(SecondEqu) + "=" + str(Ruselt) + "\n"
+   #Serching in all File
+    x = file.readlines()
+    if Holder in x:
+        Check = True
+        print("yeah i found your word in file.")
 
-#read file
-def fileworking(Fristequ,SecondEqu,Operation,Ruselt):
- file= open("Math.txt","w")
- file.write(Fristequ)
- file.write("\t")
- file.write(Operation)
- file.write("\t")
- file.write(SecondEqu)
- file.write("\t")
- file.write("=")
- file.write(str(Ruselt))
- file.write("\n")
- file.close()
+    else:
+        print("too bad there is  nothing.")
+        Check=False
+    #file.Close()
 
+
+def fileworking(Fristequ, SecondEqu, Operation, Ruselt):
+    file = open("Math.txt", "a")  # a for append
+    file.write(Fristequ)
+    file.write(Operation)
+    file.write(SecondEqu)
+    file.write("=")
+    file.write(str(Ruselt))
+    file.write("\n")
+    file.close()
+    Check = False
+
+##########End Of Working With File########
+
+##Start The Caculation##
 def call_result_sum(label_result, n1, n2):
+    global Check
     num1 = (n1.get())
     num2 = (n2.get())
-    Generate="+"
+    Generate = "+"
     result = int(num1) + int(num2)
-    fileworking(num1,num2,Generate,result)
+    FileSearch(num1, num2, Generate, result)
+
+    if Check == False:
+        #prevernting The Re-Writing
+        fileworking(num1, num2, Generate, result)
     label_result.config(text="Result is %d" % result)
-    return
+
+
 def call_result_Dif(label_result, n1, n2):
+    global Check
     num1 = (n1.get())
-    num2 =(n2.get())
-    Generate="-"
+    num2 = (n2.get())
+    Generate = "-"
     result = int(num1) - int(num2)
-    fileworking(num1,num2,Generate,result)
+    FileSearch(num1, num2, Generate, result)
+    if Check ==False:
+        #prevernting The Re-Writing
+        fileworking(num1, num2, Generate, result)
 
     label_result.config(text="Result is %d" % result)
 
-
-#Adjusting Layout
+##Ending of Caculation
+######Starting The Gui Code#####
+# Adjusting Layout
 App = tk.Tk()
 App.geometry('400x200+500+200')
 App.title('Polynomial calculator')
 
-#Varibles for Equations
+# Varibles for Equations
 number1 = tk.StringVar()
 number2 = tk.StringVar()
 
@@ -52,10 +81,9 @@ entryNum1 = tk.Entry(App, textvariable=number1).grid(row=1, column=2)
 entryNum2 = tk.Entry(App, textvariable=number2).grid(row=2, column=2)
 call_result_sum = partial(call_result_sum, labelResult, number1, number2)
 buttonSum = tk.Button(App, text="Sum", command=call_result_sum).grid(row=3, column=2)
-call_result_Dif = partial(call_result_Dif,labelResult, number1, number2)
-buttonDif = tk.Button(App,text="Substract", command=call_result_Dif).grid(row = 4,column = 2)
+call_result_Dif = partial(call_result_Dif, labelResult, number1, number2)
+buttonDif = tk.Button(App, text="Substract", command=call_result_Dif).grid(row=4, column=2)
 
-
-#App Runnig
+# App Runnig
 App.mainloop()
-##################################################End of GUI PART#################
+############################################End of GUI PART#################
